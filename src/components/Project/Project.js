915ed9ad00a1ae0531ec30e0f78d0acefed004f9
Niki Catapano <nsc9605@gmail.com>
2021-03-22 11:21:16 -0400

    add photos and set up project cards

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,24 +1,27 @@
 import React from 'react';
-// import projects from '../../pages/Portfolio/index'
+
+// import { EmpDirectory, Scheduler, Buns, Burger, EmpTracker, Luck } from '../../assets/img';
 
 
 function Project(props) {
 
     return (
         // <div className="card-container">
-        <div className="portfolio-item">
-            <img className="project-img" src={props.image} alt={props.title} />
-            <div className="info">
+        <div className="card portfolio-item p-4">
+            <img className="card-img-top" src={props.image} alt={props.title} />
+            <div className="card-body">
                 <div className="card-content info-content">
-                    <h3 className="project-title">{props.title}</h3>
-                    <p>{props.description}</p>
-                    <p>{props.tags}</p>
-                    <div className="links">
+                    <h3 className="card-title">Project{props.title}</h3>
+                    <p className="card-text">{props.description}</p>
+                    <p className="card-text">{props.tags}</p>
+                    <div className="card-links">
                         <a href={props.repo} rel='noreferrer' target="_blank">
                             <i className="fa fa-github"></i>
                         </a>
+                        <br>
+                        </br>
                         <a href={props.deploy} rel='noreferrer' target="_blank">
-                            <i className="fa fa-"></i>
+                            <i className="bi bi-window"></i>
                         </a>
                     </div>
                 </div>
--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ContainerFluid from "../../components/ContainerFluid/index";
 import Project from "../../components/Project/Project";
-import projects from "../../projects.json";
+// import projects from "../../projects.json";
+import projects from "../../utils/Projects";
+// import images from "../../assets/img"
 
 function Portfolio() {
 
   return (
     <ContainerFluid id="portfolio" title="Portfolio">
-      <div className="card">
+      <div className="">
         {projects.map((project) => (
           <Project
           project={project}
