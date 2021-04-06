95be00f02a4a0c6cf83c52d7692036a3aa43299f
Niki Catapano <nsc9605@gmail.com>
2021-04-06 15:47:57 -0400

    comment out something on home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,7 +8,7 @@ function Home() {
   return (
     <Container fluid="md">
       <div className="home-div">
-        <div subheading="Home ml-2 px-2" className="">
+        <div subheading="Home ml-2 px-2">
           <img
             src={home}
             alt="Home"
@@ -26,37 +26,40 @@ function Home() {
               &mdash; David Bowie, <cite>Madison Square Garden 1997</cite>
             </figcaption>
           </div>
-        </div>
-        <div className="px-3 m-3 objects">
-          <h3 className="indent mx-5 px-2 float-left">[ Creator ]</h3>
-          <br></br>
-          <br></br>
-          <p>Creativity can be seen in various aspects of life.</p>
-
-          <h3 className="indent mx-5 px-2 float-right">[ Problem Solver ]</h3>
-          <br></br>
-          <br></br>
-          <p>
-            Born problem solver who constantly finds challenges and must
-            accomplish them. Love learning new things and challenging myself.
-          </p>
-          <h3 className="indent mx-5 px-2 float-left"> [ Explorer ]</h3>
-          <br></br>
-          <br></br>
-          <p>
-            Love to go on spontaneous adventures and just explore this wonderful
-            city I am lucky to live in.
-          </p>
-          <h3 className="indent mx-5 px-2 float-right">[ Developer ]</h3>
-          <br></br>
-          <br></br>
-          <p>
-            Thrive on developing new Apps and really challenging myself to try
-            new things. Please navigate into the Projects section to view
-            projects with deployed link and github provided. For more
-            information about me you can click the social icons at the bottom of
-            the page or navigate to the Contact page!
-          </p>
+          {/* </div> */}
+          <div className="px-3 m-3 objects">
+            {/* <h3 className="indent mx-5 px-2 float-left">[ Creator ]</h3>
+            <br></br>
+            <br></br>
+            <br></br>
+              <p>
+                Creativity can be seen in various aspects of life.
+              </p> */}
+            <h3 className="indent mx-5 px-2 float-right">[ Problem Solver ]</h3>
+            <br></br>
+            <br></br>
+            <p>
+              Born problem solver who constantly finds challenges and must
+              accomplish them. Love learning new things and challenging myself.
+            </p>
+            <h3 className="indent mx-5 px-2 float-left"> [ Explorer ]</h3>
+            <br></br>
+            <br></br>
+            <p>
+              Love to go on spontaneous adventures and just explore this
+              wonderful city I am lucky to live in.
+            </p>
+            <h3 className="indent mx-5 px-2 float-right">[ Developer ]</h3>
+            <br></br>
+            <br></br>
+            <p>
+              Thrive on developing new Apps and really challenging myself to try
+              new things. Please navigate into the Projects section to view
+              projects with deployed link and github provided. For more
+              information about me you can click the social icons at the bottom
+              of the page or navigate to the Contact page!
+            </p>
+          </div>
         </div>
       </div>
     </Container>
