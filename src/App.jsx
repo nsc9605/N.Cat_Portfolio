643d7a68de3773ff845d7b9143a9c7c4fc1f4b1a
Niki Catapano <nsc9605@gmail.com>
2021-07-19 15:53:04 -0400

    add styles folder

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Portfolio from "./pages/Portfolio/index";
 import Contact from "./pages/Contact/index";
 // import Footer from "./components/Footer/Footer";
 // import Container from "react-bootstrap/Container";
-import './index.css';
+import './styles/index.css';
 
 function App() {
     return (
